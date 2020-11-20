3e48b3e8b279338f7236a54b30519122e3b4ab58
Leejaedeok <jdlee@jasongroup.co.kr>
2020-11-20 11:06:50 +0900

    수정기능 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  state = {
-    users: [],
-    name: "",
-    age: "",
-  };
+  constructor(props) {
+    super();
+    this.state = {
+      users: [],
+      name: "",
+      age: "",
+    };
+  }
 
   componentDidMount() {
     this.getUser();
@@ -38,26 +41,64 @@ class App extends Component {
         "Content-Type": "application/json",
       },
     }).then((res) => this.getUser());
+
     document.getElementById("name").value = "";
     document.getElementById("age").value = "";
+  };
+
+  handleUpdate = (e) => {
+    e.preventDefault();
+    const { id } = e.target;
+    const name = prompt("바꿀 이름을 입력하세요.");
+
+    if (!name) {
+      return alert("이름을 입력해주세요.");
+    }
+
+    fetch(`/users/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        name,
+      }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then((res) => this.getUser());
+  };
+
+  handleDelete = (e) => {
+    e.preventDefault();
+    const { id } = e.target;
+    fetch(`/users/${id}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then((res) => this.getUser());
   };
 
   render() {
     return (
       <div className="App">
         <h1>Users</h1>
-        <div>
-          <p>
-            이름 : <input id="name" name="name" onChange={this.handleChange} />
-          </p>
-          <p>
-            나이 : <input id="age" name="age" onChange={this.handleChange} />
-          </p>
+        <p>
+          이름 : <input id="name" name="name" onChange={this.handleChange} />
+          <br />
+          나이 : <input id="age" name="age" onChange={this.handleChange} />
+          <br />
           <button onClick={this.handleClick}>등록</button>
-        </div>
+        </p>
         {this.state.users.map((user) => (
-          <div key={user.id}>
+          <div key={user._id}>
             {user.name} {user.age}
+            <button id={user._id} onClick={this.handleUpdate}>
+              수정
+            </button>
+            <button id={user._id} onClick={this.handleDelete}>
+              삭제
+            </button>
           </div>
         ))}
       </div>
